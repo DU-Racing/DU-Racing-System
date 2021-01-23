f72b98a156bf65facc3b16e9ab8404b321dab625
asc2030 <asc2030@gmail.com>
2021-01-23 15:10:09 +0000

    code formatting

--- src/vehicle-on-receive.lua
+++ src/vehicle-on-receive.lua
@@ -1,30 +1,28 @@
 local uid = unit.getMasterPlayerId()
 
 if channel == uid .. "-splitmsg" or channel == "dur-splitmsg" then
-    mess = string.gsub(message, '|', '\\"')
+    mess = string.gsub(message, "|", '\\"')
     local part = json.decode(mess)
     if part == nil then
         system.print("Invalid message: " .. mess)
         return false
     end
-    table.insert(messageParts, { index = part["i"], content = part["content"]})
-    if part["i"] == part["len"] then 
-       local fullMessage = getCompleteMessage()
+    table.insert(messageParts, {index = part["i"], content = part["content"]})
+    if part["i"] == part["len"] then
+        local fullMessage = getCompleteMessage()
 
         if part["action"] == "save-track" then
-          saveBroadcastedTrack(fullMessage)
+            saveBroadcastedTrack(fullMessage)
         end
     end
 end
 
 if channel == raceID then
-   if message == "start" then
-      startRace()  
-   end
+    if message == "start" then
+        startRace()
+    end
 end
 
 if channel == uid .. "-registered" then
-   saveBroadcastedTrack(message)
+    saveBroadcastedTrack(message)
 end
-
-
